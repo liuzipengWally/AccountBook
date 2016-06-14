af0af2c0237c01430c6bef059ba98ca1aea2c563
liuzipeng <tracy550609334@gmail.com>
2016-06-14 13:47:55 +0800

    branch-test

--- AccountBook/app/src/main/java/com/accountbook/view/fragment/ChartFragment.java
+++ AccountBook/app/src/main/java/com/accountbook/view/fragment/ChartFragment.java
@@ -1,5 +1,6 @@
 package com.accountbook.view.fragment;
 
+import android.app.Fragment;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -7,7 +8,7 @@ import android.content.IntentFilter;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
--- AccountBook/app/src/main/java/com/accountbook/view/fragment/LoginFragment.java
+++ AccountBook/app/src/main/java/com/accountbook/view/fragment/LoginFragment.java
@@ -3,6 +3,7 @@ package com.accountbook.view.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
+
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
